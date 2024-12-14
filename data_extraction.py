b2117ea0d34c4a2ee07f44726f3c11e162757a9b
Andrew Gardner <beechroad4@hotmail.com>
2024-12-14 20:57:34 +0000

    Second commit

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,19 @@
+from database_utils import DatabaseConnector
+from sqlalchemy import create_engine, text, MetaData
+
+
 class DataExtractor():
-    pass
+    def __init__(self):        
+        pass
+    def read_rds_table(self, dbconnector, table):
+        engine = dbconnector.get_engine()
+        query = f"SELECT * FROM {table}"
+        df = pd.read_sql(query, con=connExtract.engine)
+        return df
+
+
+connExtract = DatabaseConnector('db_creds.yaml')
+
+extractor = DataExtractor()
+extractor.read_rds_table(connExtract, "legacy_users")
+#read_rds_table(connExtract)
