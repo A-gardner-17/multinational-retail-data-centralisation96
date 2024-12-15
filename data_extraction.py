793c52cc6da77f432f80e08467b8bb61e8c785bd
Andrew Gardner <beechroad4@hotmail.com>
2024-12-15 08:55:47 +0000

    Updated read table commit

--- data_extraction.py
+++ data_extraction.py
@@ -1,19 +1,21 @@
 from database_utils import DatabaseConnector
-from sqlalchemy import create_engine, text, MetaData
+from sqlalchemy import create_engine, text
+import pandas as pd
 
 
 class DataExtractor():
     def __init__(self):        
         pass
     def read_rds_table(self, dbconnector, table):
-        engine = dbconnector.get_engine()
+        engine = dbconnector.engine
         query = f"SELECT * FROM {table}"
-        df = pd.read_sql(query, con=connExtract.engine)
+        df = pd.read_sql(query, con=engine)
         return df
 
 
-connExtract = DatabaseConnector('db_creds.yaml')
+dbconnector = DatabaseConnector('db_creds.yaml')
 
 extractor = DataExtractor()
-extractor.read_rds_table(connExtract, "legacy_users")
+df = extractor.read_rds_table(dbconnector, "legacy_users")
+print(df.head())
 #read_rds_table(connExtract)
