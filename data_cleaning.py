b7509d2e99857267a5830556489dcbc0ce2a60b3
Andrew Gardner <beechroad4@hotmail.com>
2024-12-15 20:51:15 +0000

    Updated read table commit

--- data_cleaning.py
+++ data_cleaning.py
@@ -2,5 +2,5 @@ class  DataCleaning():
     def __init__(self):
         pass
 
-    def clean_user_data(self):
+    def clean_user_data(self,df):
         pass
--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,8 @@
 from database_utils import DatabaseConnector
 from sqlalchemy import create_engine, text
 import pandas as pd
+from data_cleaning import DataCleaning
+import numpy as np
 
 
 class DataExtractor():
@@ -12,10 +14,56 @@ class DataExtractor():
         df = pd.read_sql(query, con=engine)
         return df
 
-
 dbconnector = DatabaseConnector('db_creds.yaml')
 
 extractor = DataExtractor()
 df = extractor.read_rds_table(dbconnector, "legacy_users")
 print(df.head())
-#read_rds_table(connExtract)
+print(df.shape)
+#rows_with_nulls = df[df.isnull().any(axis=1)]
+#print(rows_with_nulls)
+rows_with_nulls = df[df.isnull().any(axis=1)]
+print(rows_with_nulls)
+print(df.isnull().values.any())
+# Replace "NULL" with NaN (pandas recognizes NaN as the null data type)
+df.replace("NULL", np.nan, inplace=True)
+print(df.isnull().values.any())
+
+# Count null values in each column
+print(df.isnull().sum())
+
+# Get rows with null values
+rows_with_nulls = df[df.isnull().any(axis=1)]
+print(rows_with_nulls)
+
+# Drop rows with any null values
+df_cleaned = df.dropna()
+
+print(df_cleaned.shape)
+
+# Convert 'join_data' column to datetime
+df_cleaned['join_date'] = pd.to_datetime(df_cleaned['join_date'], format='%Y-%m-%d', errors='coerce')
+
+#df_cleaned['join_date'] = pd.to_datetime(df_cleaned['join_date'], errors='coerce')
+invalid_dates = df_cleaned[df_cleaned['join_date'].isna()]
+print("Rows with invalid dates:")
+print(invalid_dates)
+
+df_cleaned2 = df_cleaned.dropna()
+
+print(df_cleaned2.shape)
+input()
+
+# Validate 'join_date' column
+#df_cleaned['date_of_birth'] = pd.to_datetime(df_cleaned['date_of_birth'], format='%Y-%m-%d', errors='coerce')
+#df_cleaned['date_of_birth'] = pd.to_datetime(df_cleaned['date_of_birth'], errors='coerce')
+
+# Find invalid dates
+invalid_dates = df_cleaned[df_cleaned['date_of_birth'].isna()]
+print("Rows with invalid dates:")
+print(invalid_dates)
+
+cleaning = DataCleaning()
+
+df = cleaning.clean_user_data(df)
+
